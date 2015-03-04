e918b8f817e65430e98232a80be8095d3b658ba8
Tyler Renelle <tylerrenelle@gmail.com>
2015-03-04 10:48:14 -0700

    feat(invite): move invites into bar-details, as dedicated ctrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,6 +11,50 @@ angular.module('app.controllers', [])
   $scope.bar = bar; //in state.resolve, required for view title
 })
 
+.controller('InviteFriendsCtrl', function($scope, ContactsService, $ionicModal){
+  $scope.data = {
+    selectedContacts : []
+  };
+
+  $scope.pickContact = function() {
+    ContactsService.pickContact().then(
+      function(contact) {
+        $scope.data.selectedContacts.push(contact);
+        console.log("Selected contacts=");
+        console.log($scope.data.selectedContacts);
+      },
+      function(failure) {
+        console.log("Bummer.  Failed to pick a contact");
+      }
+    );
+  }
+
+  $ionicModal.fromTemplateUrl('templates/invite-friends.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+})
+
 .controller('FriendsCtrl', function($scope, Friends) {
   //$scope.friends = Friends.all();
 })
@@ -25,29 +69,3 @@ angular.module('app.controllers', [])
   $scope.user.$bindTo($scope, "fbUser");
   $scope.Auth = Auth;
 })
-
-
-.controller("AppCtrl", ['$scope', 'ContactsService', function($scope, ContactsService) {
-
-    $scope.data = {
-      selectedContacts : []
-    };
-
-    $scope.pickContact = function() {
-
-      ContactsService.pickContact().then(
-        function(contact) {
-          $scope.data.selectedContacts.push(contact);
-          console.log("Selected contacts=");
-          console.log($scope.data.selectedContacts);
-
-        },
-        function(failure) {
-          console.log("Bummer.  Failed to pick a contact");
-        }
-      );
-
-    }
-
-
-  }])
