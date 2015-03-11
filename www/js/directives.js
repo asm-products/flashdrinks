c1b779d45a64ff3d791a094d0ae7fa97876e44ef
Lisa <lisamarie7073@gmail.com>
2015-03-10 23:29:30 -0600

    feat(chat): move chat bar up above the messages.

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,6 +1,6 @@
 angular.module('app.directives', [])
 
-.directive('chatList', function (ref, Auth, $firebase, $compile) {
+.directive('chatList', function (ref, Auth, $firebase) {
 
   // For basic cases of /firebase/chats/bar-1, chatId will be a string (bar-1). For cases of two or more users chatting,
   // we want a unique id combining the two users into a chatroom - so we concat their userIds. To ensure it turns out the
@@ -19,19 +19,7 @@ angular.module('app.directives', [])
       type: '@'
     },
     link: function(scope, element, attrs) {
-      var user, cid, tpl, fid;
-
-      // What's going on here: <ion-footer-bar> *has* to be in the HTML at run-time. I've tried using prelink functions with
-      // negative priorities, no cigar (a bit of that here http://forum.ionicframework.com/t/ionic-list-gets-overlapped-for-ion-footer-bar/12666/2).
-      // So, I'm using and empty <ion-footer-bar> in the html, then decorating it here
-      // (template ideas: https://github.com/mhartington/Ionic-Chat/blob/master/www/index.html, http://codepen.io/rossmartin/pen/XJmpQr)
-      tpl =
-        '<label class="item-input-wrapper">'+
-        '<input type="text" placeholder="Type your message" ng-model="data.text" />'+
-        '</label>'+
-        '<button ng-click="send()" class="button button-icon icon ion-android-send footer-btn" ng-disabled="!data.text" />';
-      element.parent().parent().parent().parent().parent().find('ion-footer-bar').addClass('bar-stable item-input-inset').append($compile(tpl)(scope));
-
+      var user, cid, fid;
       user = Auth.getUser();
       cid = chatId(scope.chatId);
       scope.chats = $firebase(ref.chats.child(cid)).$asArray();
