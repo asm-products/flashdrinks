5c0d158292991ea368fa58fe24133f69d971d121
Tyler Renelle <tylerrenelle@gmail.com>
2015-03-20 15:35:06 -0600

    refactor(invite): nix some unecessary boilerplate

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -64,30 +64,15 @@ angular.module('app.controllers', [])
   }
 
   // ---- MODAL ----
-  // TODO refactor this out somewhere, this is ugly. Boilerplate from Ionic samples, is all this really necessary?
   $ionicModal.fromTemplateUrl('templates/friends/invite.html', {
     scope: $scope,
     animation: 'slide-in-up'
   }).then(function(modal) {
     $scope.modal = modal;
   });
-  $scope.openModal = function() {
-    $scope.modal.show();
-  };
-  $scope.closeModal = function() {
-    $scope.modal.hide();
-  };
   //Cleanup the modal when we're done with it!
   $scope.$on('$destroy', function() {
     $scope.modal.remove();
-  });
-  // Execute action on hide modal
-  $scope.$on('modal.hidden', function() {
-    // Execute action
-  });
-  // Execute action on remove modal
-  $scope.$on('modal.removed', function() {
-    // Execute action
   });
 })
 
