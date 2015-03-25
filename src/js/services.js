c9185d7887d5b5f4d22a26b70341bc28faf69564
Tyler Renelle <tylerrenelle@gmail.com>
2015-03-25 15:08:44 -0600

    Revert "feat(upload): try using boilerplate from"
    This reverts commit 8703525.

--- src/js/services.js
+++ src/js/services.js
@@ -270,21 +270,56 @@ angular.module('app.services', [])
     };
   }])
 
-.factory('ImageUploadService', function($q, SERVER) {
-  var service = {
-    uploadImage: function (imageURI) {
-      var deferred = $q.defer();
-      var options = new FileUploadOptions();
-      options.fileKey = "file";
-      options.fileName = imageURI.substr(imageURI.lastIndexOf('/') + 1);
-      options.mimeType = "image/jpeg";
-      var params = new Object();
-      options.params = params;
-      options.chunkedMode = false;
-      var ft = new FileTransfer();
-      ft.upload(imageURI, SERVER+'/s3-upload', deferred.resolve, deferred.reject, options);
-      return deferred.promise;
-    }
-  }
+.factory('ImageUploadService', function($q, $ionicLoading, $cordovaFileTransfer, SERVER) {
+  var service = {};
+  service.uploadImage = uploadImage;
   return service;
+
+  function uploadImage(imageURI) {
+    var deferred = $q.defer();
+    var fileSize;
+    var percentage;
+
+    // Find out how big the original file is
+    window.resolveLocalFileSystemURL(imageURI, function(fileEntry) {
+      fileEntry.file(function(fileObj) {
+        fileSize = fileObj.size;
+
+        // Display a loading indicator reporting the start of the upload
+        $ionicLoading.show({template : 'Uploading Picture : ' + 0 + '%'});
+
+        // Trigger the upload
+        uploadFile();
+      });
+    });
+
+    function uploadFile() {
+      $cordovaFileTransfer
+        .upload(SERVER + '/s3-upload', imageURI)
+        .then(function(result) {
+          // Let the user know the upload is completed
+          $ionicLoading.show({template : 'Upload Completed', duration: 1000});
+
+          // Result has a "response" property that is escaped
+          // FYI: The result will also have URLs for any new images generated with
+          // eager transformations
+          var response = JSON.parse(decodeURIComponent(result.response));
+          deferred.resolve(response);
+
+        }, function(err) {
+          console.dir(err);
+          // Uh oh!
+          $ionicLoading.show({template : 'Upload Failed', duration: 3000});
+          deferred.reject(err);
+        }, function (progress) {
+
+          // The upload plugin gives you information about how much data has been transferred
+          // on some interval.  Use this with the original file size to show a progress indicator.
+          percentage = Math.floor(progress.loaded / fileSize * 100);
+          $ionicLoading.show({template : 'Uploading Picture : ' + percentage + '%'});
+        });
+    }
+
+    return deferred.promise;
+  }
 })
